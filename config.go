81fcf49beb5ea9e2a45eb7a95ccf74a0914b335b
Farzin Far <farzin.farzanehnia@gmail.com>
2024-09-17 19:10:32 +0330

    refactor: change load signature

--- config.go
+++ config.go
@@ -2,21 +2,64 @@ package sercon
 
 import (
 	"context"
+	"fmt"
 	"github.com/47monad/sercon/go-gen/svcconf"
 	"github.com/joho/godotenv"
 )
 
-func Load(ctx context.Context, configFileName string, envFileName string) (*svcconf.ServiceConfig, error) {
-	if envFileName != "" {
-		err := godotenv.Load(envFileName)
+type LoadOptions struct {
+	ctx        context.Context
+	configPath string
+	envPath    string
+}
+
+type LoadOption func(*LoadOptions)
+
+func Load(opts ...LoadOption) (*svcconf.ServiceConfig, error) {
+	_opts := prepareOptions(opts)
+	if _opts.configPath == "" {
+		return nil, fmt.Errorf("missing config path")
+	}
+	if _opts.envPath != "" {
+		err := godotenv.Load()
 		if err != nil {
 			return nil, err
 		}
 	}
-	conf, err := svcconf.LoadFromPath(ctx, configFileName)
+	conf, err := svcconf.LoadFromPath(_opts.ctx, _opts.configPath)
 	if err != nil {
 		return nil, err
 	}
 
 	return conf, nil
+}
+
+func prepareOptions(options []LoadOption) *LoadOptions {
+	opts := &LoadOptions{
+		ctx: context.Background(),
+	}
+
+	for _, option := range options {
+		option(opts)
+	}
+
+	return opts
+}
+
+func WithContext(ctx context.Context) LoadOption {
+	return func(o *LoadOptions) {
+		o.ctx = ctx
+	}
+}
+
+func WithConfigPath(path string) LoadOption {
+	return func(o *LoadOptions) {
+		o.configPath = path
+	}
+}
+
+func WithEnvPath(path string) LoadOption {
+	return func(o *LoadOptions) {
+		o.envPath = path
+	}
 }
