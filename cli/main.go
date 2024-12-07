1e9d36291483ee0bcf312f9c7fac7b94cec4aa93
Farzin Far <farzinfweb@gmail.com>
2024-12-07 18:27:25 +0330

    feat: add cli tool

---
+++ cli/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/47monad/sercon"
+)
+
+func main() {
+	var (
+		configBasePath string
+		pklPath        string
+		envPath        string
+		outputPath     string
+	)
+
+	flag.StringVar(&configBasePath, "base", "", "config base path")
+	flag.StringVar(&pklPath, "pkl", "", "pkl file name")
+	flag.StringVar(&envPath, "env", "", "env file name")
+	flag.StringVar(&outputPath, "output", "", "output file name")
+
+	flag.Parse()
+
+	if outputPath == "" {
+		outputPath = ".sercon/app.json"
+	}
+
+	if configBasePath == "" {
+		configBasePath = "./config"
+	}
+
+	if pklPath == "" {
+		pklPath = "app.pkl"
+	}
+
+	if envPath == "" {
+		envPath = ".env"
+	}
+
+	fullPKLPath := fmt.Sprintf("%s/%s", configBasePath, pklPath)
+	fullEnvPath := fmt.Sprintf("%s/%s", configBasePath, envPath)
+
+	err := sercon.Build(fullPKLPath, outputPath, fullEnvPath)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Config was built inside %s", outputPath)
+}
